442145fbbe9c0a9e354fd810c52291aee784d0af
5rthwhiin <hisy2059@naver.com>
2021-07-05 18:58:19 +0900

    dp + tree

--- 2533/2533.cpp
+++ 2533/2533.cpp
@@ -12,48 +12,26 @@ typedef long long ll;
 using namespace std;
 
 vector<vector<int> > tree;
-int dp[1000001][2];			   //dp[n][k] : n이 k(얼리어답트)일때, 필요한 최소 얼리어답트수
+int dp[1000001][2];			   //dp[n][k] : n이 k(얼리)일때, 필요한 최소 얼리어답트수
 int solve(int n, int p, int k) //p = 부모
 {
 	int &ret = dp[n][k];
 	if (ret != -1)
 		return ret;
+	ret = k;
 	int len = tree[n].size();
-	vector<int> child_vec; // 자식들을 저장할 벡터
 	for (int i = 0; i < len; i++)
 	{
 		int there = tree[n][i];
 		if (there == p)
 			continue;
-		child_vec.push_back(there);
+		if (k == 0) //내가 얼리가 아니면 자식들은 모두 얼리여야함
+			ret += solve(there, n, 1);
+		else if (k == 1) //내가 얼리면 자식들은 얼리일떄와 아닐때 중 최소를 선택
+			ret += min( solve(there, n, 1), solve(there, n, 0));
 	}
-	if (child_vec.size() == 0) //자식이 존재하지 않음 ; leaf 노드
-	{
-		dp[n][0] = 0;
-		dp[n][1] = 1;
-		return ret;
-	}
-	len = child_vec.size();
-	ret = 0;
-	if (k == 0) //자신이 얼리어답트가 아니라면 자식들은 모두 얼리어답트여야함
-	{
-		for (int i = 0; i < len; i++)
-		{
-			int child = child_vec[i];
-			ret += solve(child, n, 1);
-		}
-		return ret;
-	}
-	else //자신이 얼리어답트라면 자식들이 얼리어답트일때와 아닐때중 최소의 경우를 고른다.
-	{
-		for (int i = 0; i < len; i++)
-		{
-			int child = child_vec[i];
-			ret += min(solve(child, n, 1), solve(child, n, 0));
-		}
-		ret += 1;
-		return ret;
-	}
+	/*자식이 없는 경우: 자신이 얼리면 1, 아니면 0*/
+	return ret;
 }
 int main()
 {
